0a751749d5a5dca9554f7177e5f1edd3709fb1a3
sakiadachi <sakiadachi.88@gmail.com>
2024-05-09 11:38:06 +0900

    Changed urls

--- hatsuon/hatsuon/urls.py
+++ hatsuon/hatsuon/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
 
+from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
-    path('', include('hatsuon_app.urls')),
-    path('api-auth/' ,include('rest_framework.urls',namespace='rest_framework'))
+    path("api/v1/", include("hatsuon_app.urls")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("admin/", admin.site.urls),
 ]
