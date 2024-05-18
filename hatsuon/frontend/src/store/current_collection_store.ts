7e8c9f009a705a9a6272dcfdbe4cf18e5e36f96f
sakiadachi <sakiadachi.88@gmail.com>
2024-05-19 00:16:32 +0900

    current_collection_store id -> uuid

--- hatsuon/frontend/src/store/current_collection_store.ts
+++ hatsuon/frontend/src/store/current_collection_store.ts
@@ -13,7 +13,7 @@ const collection_title: Readable<string> = derived(
 );
 const collection_id: Readable<string> = derived(
   current_collection,
-  ($current_collection) => $current_collection?.id || "",
+  ($current_collection) => $current_collection?.uuid || "",
 );
 
 /**
@@ -26,7 +26,7 @@ const phrase_title: Readable<string> = derived(
 );
 const phrase_id: Readable<string> = derived(
   current_phrase,
-  ($current_phrase) => $current_phrase?.id || "",
+  ($current_phrase) => $current_phrase?.uuid || "",
 );
 
 export default {
