f41fe1bf100ea87a493f3fe5ffaeda031c41e743
sakiadachi <sakiadachi.88@gmail.com>
2024-05-19 23:14:19 +0900

    use process.env

--- hatsuon/frontend/vite.config.js
+++ hatsuon/frontend/vite.config.js
@@ -4,7 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { manifest: true },
-  base: import.meta.PROD === "production" ? "/static/" : undefined,
-  // base: "/static/",
+  base: process.env.PROD ? "/static/" : undefined,
   plugins: [svelte()],
 });
