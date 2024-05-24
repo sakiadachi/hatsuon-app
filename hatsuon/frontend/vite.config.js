4142e611085eb345ca215e3a8fd2220141722ffe
sakiadachi <sakiadachi.88@gmail.com>
2024-05-24 19:12:35 +0900

    changed vite.config.js

--- hatsuon/frontend/vite.config.js
+++ hatsuon/frontend/vite.config.js
@@ -3,7 +3,12 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true },
-  base: process.env.PROD ? "/static/" : undefined,
+  // build: { manifest: true },
+  // base: process.env.PROD ? "/static/" : undefined,
   plugins: [svelte()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
