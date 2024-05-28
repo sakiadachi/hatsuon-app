eb4b3ef5be35d6a65990e5276430019a009350d8
sakiadachi <sakiadachi.88@gmail.com>
2024-05-28 13:25:26 +0900

    vite.config

--- hatsuon/svelte-kit/vite.config.ts
+++ hatsuon/svelte-kit/vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		strictPort: true,
+		port: 5173
+	}
 });
