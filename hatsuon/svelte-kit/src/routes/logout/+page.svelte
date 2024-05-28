f4dd18b2966cc8b919a573c777015ea9192aac12
sakiadachi <sakiadachi.88@gmail.com>
2024-05-28 17:06:21 +0900

    Add logout page

---
+++ hatsuon/svelte-kit/src/routes/logout/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { beforeUpdate } from "svelte";
+  import auth_store from "$lib/store/auth_store";
+
+  const { logout } = auth_store;
+
+  let isLoading: boolean = false;
+  let logoutSuccess: boolean = false;
+
+  beforeUpdate(async () => {
+    isLoading = true;
+    logoutSuccess = await logout();
+    isLoading = false;
+  });
+</script>
+
+<div>
+  {#if isLoading}
+    <p>Loading...</p>
+  {/if}
+  {#if logoutSuccess}
+    <p>You have successfully logged out.</p>
+    <a href="/login">Go to Login</a>
+  {/if}
+</div>
