efa6fdba184ea6e0f36375cf27111b9eb419552c
sakiadachi <sakiadachi.88@gmail.com>
2024-06-06 18:21:50 +0900

    build static site setting

--- svelte-kit/src/routes/+page.ts
+++ svelte-kit/src/routes/+page.ts
@@ -1,18 +1,3 @@
-import type { PageLoad } from "./$types";
-import { collections } from "./data";
-
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-
-export const load: PageLoad = () => {
-  // Can't make request using CSRF token because its stored in browser cookie!
-  // const result = await fetchApi("api/v1/collections/");
-  // const collections:Collection[] = await result.json();
-
-  return {
-    collections: collections.map((c) => ({
-      id: c.id,
-      title: c.title,
-    })),
-  };
-};
+export const prerender = true;
--- svelte-kit/src/routes/collection/[id]/phrase/[id]/+page.ts
+++ svelte-kit/src/routes/collection/[id]/phrase/[id]/+page.ts
@@ -1,3 +1,5 @@
+export const prerender = false;
+
 export type AudioSrc = {
   src: string;
   name: string;
